2aba39d8e5b959dbc48bed8f3766d878929c7990
Jax <jax_lewis1@baylor.edu>
2023-02-08 13:04:46 -0600

    classwork

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -11,9 +11,30 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
+Display report for all universities that have a graduation rate for Women over 75%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
 
 
 """
+
+import json
+
+infile = open("school_data.json", "r")
+
+schools = json.load(infile)
+conference_schools = [372, 108, 107, 130]
+
+print(type(schools))
+
+# how many schools are in this file?
+print(len(schools))
+
+# print name of school, graduation of conferences, above 80% grad rate for women
+for school in schools:  # "school" represents the dictionary
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 80:
+            print(school["instnm"])
+            print(school["Graduation rate  women (DRVGR2020)"])
+            print()
+            print()
